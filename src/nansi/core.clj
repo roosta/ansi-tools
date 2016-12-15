d5ae5c3fce0da483ae65029f15603383aba78d14
Daniel Berg <mail@roosta.sh>
2016-12-15 20:17:42 +0100

    Begin experimenting

--- src/nansi/core.clj
+++ src/nansi/core.clj
@@ -1,7 +1,41 @@
 (ns nansi.core
+  (:require [clojure.string :as s])
   (:gen-class))
 
+(def symbols
+  {"┌" "&#9484;"
+   "┐" "&#9488;"
+   "└" "&#9492;"
+   "┘" "&#9496;"
+   "│" "&#9474;"
+   "─" "&#9472;"
+   "░" "&#9617;"
+   "█" "&#9608;"
+   "▀" "&#9600;"
+   "▄" "&#9604;"})
+
+(def pairs
+  {"├" "┤"
+   "┤" "├"
+   "┌" "┐"
+   "┐" "┌"
+   "└" "┘"
+   "┘" "└"})
+
+;; (println (first (s/split (slurp "partial.txt") #"\n")))
+
+(defn replace-char
+  []
+  (let [ascii (s/split (slurp "partial.txt") #"\n")]
+    (for [l ascii
+          c l]
+      c
+      )
+    #_(println (ffirst ascii)))
+  )
+
+(println (replace-char))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hi!"))
