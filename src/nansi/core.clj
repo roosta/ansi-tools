6e9f17d49d275f4d2fac0ffed78a87596ea2b870
Daniel Berg <mail@roosta.sh>
2016-12-18 22:41:44 +0100

    Bleh

--- src/nansi/core.clj
+++ src/nansi/core.clj
@@ -22,19 +22,48 @@
    "└" "┘"
    "┘" "└"})
 
-;; (println (first (s/split (slurp "partial.txt") #"\n")))
+(def regex
+  #"[├┘└┐┌┤]")
 
+;; (conj "zzzz" "a")
 (defn replace-char
   []
-  (let [ascii (s/split (slurp "partial.txt") #"\n")]
-    (for [l ascii
-          c l]
-      c
-      )
-    #_(println (ffirst ascii)))
+  (let [ascii (s/split-lines (slurp "partial.txt"))]
+    (reduce
+     (fn [acc line]
+       (let [r (apply str (reverse line))
+             c (s/replace r regex #(get pairs %))]
+         (conj acc c)))
+     '()
+     ascii)))
+
+
+(def lines (reverse (replace-char)))
+(def longest-length (-> (sort-by count lines)
+                        last
+                        count))
+(defn get-padding
+  []
+  (for [line lines
+        :let [padding-length (- longest-length (count line))]]
+    (take padding-length (repeatedly (fn [] " ")))))
+
+(def padding (get-padding))
+
+(defn join-line-padding
+  []
+  (map-indexed
+   (fn [idx line]
+     (conj (nth padding idx) line))
+   lines)
   )
 
-(println (replace-char))
+;; (println (join-line-padding))
+;; (println lines)
+
+(doseq [l (join-line-padding)]
+  (println l))
+;; (println (join-line-padding))
 
 (defn -main
   [& args]
