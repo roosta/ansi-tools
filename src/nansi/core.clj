6cbf7b967508f49be107c421e1492c1645c598b2
Daniel Berg <mail@roosta.sh>
2016-12-19 19:55:48 +0100

    Cleanup

--- src/nansi/core.clj
+++ src/nansi/core.clj
@@ -25,7 +25,6 @@
 (def regex
   #"[├┘└┐┌┤]")
 
-;; (conj "zzzz" "a")
 (defn replace-char
   []
   (let [ascii (s/split-lines (slurp "partial.txt"))]
@@ -37,36 +36,28 @@
      '()
      ascii)))
 
-
 (def lines (reverse (replace-char)))
 (def longest-length (-> (sort-by count lines)
                         last
                         count))
+
 (defn get-padding
   []
   (for [line lines
         :let [padding-length (- longest-length (count line))]]
-    (apply str (repeat padding-length \space))
-    #_(take padding-length (repeatedly (fn [] " ")))))
-
-(def padding (get-padding))
+    (apply str (repeat padding-length \space))))
 
 (defn join-line-padding
   []
-  (map-indexed
-   (fn [idx line]
-     (str (nth padding idx) line))
-   lines)
-  )
+  (let [padding (get-padding)]
+    (map-indexed
+     (fn [idx line]
+       (str (nth padding idx) line))
+     lines)))
 
-;; (println (join-line-padding))
-;; (println lines)
-
-;; (println (first padding))
-(doseq [l (join-line-padding)]
-  (println l))
-;; (println (join-line-padding))
 
 (defn -main
   [& args]
-  (println "Hi!"))
+  (doseq [l (join-line-padding)]
+    (println l))
+  )
