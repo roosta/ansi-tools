810950f46eb996d1852a56ab0a489c4a280a877c
Daniel Berg <mail@roosta.sh>
2016-12-20 09:20:15 +0100

    Add tools.cli to deps
    https://github.com/clojure/tools.cli

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]]
+  :dependencies [[org.clojure/tools.cli "0.3.5"]
+                 [org.clojure/clojure "1.9.0-alpha14"]]
   :main ^:skip-aot nansi.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
